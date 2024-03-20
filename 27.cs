f0a934edd9ed88274f2824017daee49b7022c9f7
ytyre <121445001+ytyre@users.noreply.github.com>
2024-03-20 22:56:07 +0400

    Create 27.cs

---
+++ 27.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int newIndex = 0; 
+        for(int i = 0; i < nums.Length; i++){
+            if(nums[i] != val){
+                nums[newIndex] = nums[i];
+                newIndex++;
+            }
+        }
+
+        return newIndex;
+    }
+}
