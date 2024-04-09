32140385425051bb369903454c42bd896d2c9c81
ytyre <121445001+ytyre@users.noreply.github.com>
2024-04-09 21:56:27 +0400

    Create 1700.cs

---
+++ 1700.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int CountStudents(int[] students, int[] sandwiches) { 
+        int[] count = new int[2]; 
+        foreach (int student in students)
+            count[student]++;
+        
+        foreach (int sandwich in sandwiches) {
+            if (count[sandwich] == 0)
+                break; 
+            count[sandwich]--;
+        }      
+        return count[0] + count[1]; 
+    }
+}
